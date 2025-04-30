4c1a637facdb05145208d7cbbda711ec00826a98
Kahan Patel <kahanhp18@outlook.com>
2025-04-30 10:33:55 -0400

    Add Hyprland conf files + Waybar conf files

--- hypr/hypr/refresh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Refresh Waybar by sending SIGUSR2
-if pgrep waybar > /dev/null; then
-    pkill -SIGUSR2 waybar
-    echo "Waybar refreshed."
-else
-    echo "Waybar is not running."
-fi
