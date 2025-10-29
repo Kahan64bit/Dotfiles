8ace04666a15bbe4c215f96391d873227e42c06b
Kahan Patel <106117072+Kahan64bit@users.noreply.github.com>
2025-10-29 02:58:50 +0000

    Add 'pyright' to ensure_installed list

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "cssls", "clangd"}, -- visit https://github.com/williamboman/mason-lspconfig.nvim for lsp list
+                ensure_installed = { "lua_ls", "cssls", "clangd", "pyright"}, -- visit https://github.com/williamboman/mason-lspconfig.nvim for lsp list
             })
         end,
     },
